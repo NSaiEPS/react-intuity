6effb13eb308d7153878d7180198de3f407d98cc
NSaiEPS <sainizameps@gmail.com>
2025-09-04 16:37:10 +0530

    auto pay not visible till admin allow

--- src/pages/dashboard/auto-pay/page.tsx
+++ src/pages/dashboard/auto-pay/page.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 
 //import { companySlugs, config } from "@/config";
 import AutoPayDetails from "@/components/dashboard/customer/auto-pay-details";
+import { useSelector } from "react-redux";
+import { RootState } from "@/state/store";
+import { getLocalStorage } from "@/utils/auth";
+import { Navigate } from "react-router";
 
 //export const metadata = {
 //   title: `Auto Pay  - ${config.site.name}`,
@@ -10,5 +14,16 @@ import AutoPayDetails from "@/components/dashboard/customer/auto-pay-details";
 //   return companySlugs.map((company) => ({ company }));
 // }
 export default function AutoPayPage(): React.JSX.Element {
+  const dashBoardInfo = useSelector(
+    (state: RootState) => state?.DashBoard?.dashBoardInfo
+  );
+  const companyDetails: any = getLocalStorage("intuity-company");
+
+  const { allow_auto_payment } =
+    dashBoardInfo?.body?.company || companyDetails || {};
+
+  if (allow_auto_payment !== 1) {
+    return <Navigate to={"/"} replace />;
+  }
   return <AutoPayDetails />;
 }
