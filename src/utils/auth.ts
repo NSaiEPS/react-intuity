6c3717f14b40cf33e59c45314f069eb757c95fbb
NSaiEPS <sainizameps@gmail.com>
2025-09-04 12:57:24 +0530

    clean code

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,24 +1,3 @@
-// export const setLocalStorage = (key, value) => localStorage.setItem(key, value);
-// export const getLocalStorage = (key) => localStorage.getItem(key);
-// export const removeLocalStorage = (token) => localStorage.removeItem(token);
-// export const clearLocalStorage = () => localStorage.clear();
-// export const updateLocalStorageValue = (storageKey, targetKey, newValue) => {
-//   try {
-//     const storedData = localStorage.getItem(storageKey);
-//     if (!storedData) {
-//       console.warn(`No data found in localStorage for key: ${storageKey}`);
-//       return;
-//     }
-
-//     const parsedData = JSON.parse(storedData);
-//     parsedData[targetKey] = newValue;
-
-//     localStorage.setItem(storageKey, JSON.stringify(parsedData));
-//   } catch (error) {
-//     console.error('Error updating localStorage:', error);
-//   }
-// };
-
 import secureLocalStorage from "react-secure-storage";
 
 export const setLocalStorage = (key: string, value: any) => {
--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -3,7 +3,6 @@
 import { getCurrentCompanySlug } from ".";
 
 export const paths = {
-  // home: (company?: string) => (company ? `/${company}` : '/'),
   home: (company?: string) =>
     company ? `/${company}/dashboard` : "/intuityfe/dashboard",
 
@@ -12,9 +11,7 @@ export const paths = {
     signUp: (company?: string) => (company ? `/sign-up` : "/sign-up"),
     resetPassword: (company?: string) =>
       company ? `/reset-password` : "/reset-password",
-    // confirmInfo: (company?: string) =>
-    //   company ? `/confirm-information` : '/confirm-information',
-    // confirmInfo: '/auth/confirm-information',
+
     confirmInfo: (company?: string) =>
       company
         ? `/${company}/confirm-information`
@@ -25,52 +22,10 @@ export const paths = {
     newLogin: (company?: string) => (company ? `/login-${company}` : "/login"),
   },
 
-  // dashboard: {
-  //   overview: (company?: string) => (company ? `/${company}/dashboard` : '/intuityfe/dashboard'),
-  //   account: (company?: string) => (company ? `/${company}/dashboard/account` : '/intuityfe/dashboard/account'),
-  //   // payNow: (company?: string) => (company ? `/${company}/dashboard/pay-now` : '/intuityfe/dashboard/pay-now'),
-  //   payNow: (company?: string) => {
-  //     console.log(company, getCurrentCompanySlug(), 'pathPartspathParts');
-  //     const slug = company ? company : getCurrentCompanySlug();
-  //     return slug ? `/${slug}/dashboard/pay-now` : '/intuityfe/dashboard/pay-now';
-  //   },
-  //   paperless: (company?: string) => (company ? `/${company}/dashboard/paperless` : '/intuityfe/dashboard/paperless'),
-  //   autoPay: (company?: string) => (company ? `/${company}/dashboard/auto-pay` : '/intuityfe/dashboard/auto-pay'),
-  //   paymentMethods: (company?: string) =>
-  //     company ? `/${company}/dashboard/payment-methods` : '/intuityfe/dashboard/payment-methods',
-  //   priorBills: (company?: string) =>
-  //     company ? `/${company}/dashboard/prior-bills` : '/intuityfe/dashboard/prior-bills',
-  //   usageHistory: (company?: string) =>
-  //     company ? `/${company}/dashboard/usage-history` : '/intuityfe/dashboard/usage-history',
-  //   service: (company?: string) => (company ? `/${company}/dashboard/service` : '/intuityfe/dashboard/service'),
-  //   settings: (company?: string) => (company ? `/${company}/dashboard/settings` : '/intuityfe/dashboard/settings'),
-  //   stopService: (company?: string) =>
-  //     company ? `/${company}/dashboard/stop-service` : '/intuityfe/dashboard/stop-service',
-  //   notificationSettings: (company?: string) =>
-  //     company ? `/${company}/dashboard/notification-settings` : '/intuityfe/dashboard/notification-settings',
-  //   linkAccount: (company?: string) =>
-  //     company ? `/${company}/dashboard/link-account` : '/intuityfe/dashboard/link-account',
-  // },
-  // dashboard: {
-  //   overview: '/dashboard',
-  //   account: '/dashboard/account',
-  //   'pay-now': '/dashboard/pay-now',
-  //   paperless: '/dashboard/paperless',
-  //   'auto-pay': '/dashboard/auto-pay',
-  //   'payment-methods': '/dashboard/payment-methods',
-  //   'prior-bills': '/dashboard/prior-bills',
-  //   'usage-history': '/dashboard/usage-history',
-  //   service: '/dashboard/service',
-  //   settings: '/dashboard/settings',
-  //   'stop-service': '/dashboard/stop-service',
-  //   'notification-settings': '/dashboard/notification-settings',
-  //   'link-account': '/dashboard/link-account',
-  // },
-
   dashboard: {
     overview: (company?: string) => {
       const slug = company ?? getCurrentCompanySlug();
-      // console.log(slug, 'slugssss');
+
       return slug ? `/${slug}/dashboard` : "/intuityfe/dashboard";
     },
     account: (company?: string) => {
