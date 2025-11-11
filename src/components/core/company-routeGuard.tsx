37c91ba0ed938d2a1c5a4d3c2bbe099cbfc4a14b
NSaiEPS <sainizameps@gmail.com>
2025-11-11 21:16:38 +0530

    dashboard redirect done

--- src/components/core/company-routeGuard.tsx
+++ src/components/core/company-routeGuard.tsx
@@ -1,13 +1,22 @@
 // CompanyRouteGuard.tsx
+import {
+  getConfirmInfo,
+  getUserInfoByToken,
+} from "@/state/features/accountSlice";
 import { getLocalStorage } from "@/utils/auth";
+import { decryptFromPHP } from "@/utils/decryptHelper";
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Navigate, Outlet, useLocation, useParams } from "react-router-dom";
 import secureLocalStorage from "react-secure-storage";
+import { useLoading } from "./skeletion-context";
 
 export default function CompanyRouteGuard() {
   const user = getLocalStorage("intuity-user");
   const storedCompanyId = secureLocalStorage.getItem("intuity-companyId");
   const aliasUser: any = getLocalStorage("alias-details");
+  const dispatch = useDispatch();
+  const { setContextLoading } = useLoading();
 
   const { company } = useParams();
   const { pathname } = useLocation();
@@ -15,6 +24,65 @@ export default function CompanyRouteGuard() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
+  const { search } = useLocation();
+  if (search.includes("token=")) {
+    const token = () => {
+      const match = search.match(/[?&]token=([^&]*)/);
+      console.log(search, match);
+      if (!match) return null;
+      // Preserve '+' (used in AES base64)
+      const raw = match[1].replace(/\+/g, "%2B");
+      try {
+        return decodeURIComponent(raw);
+      } catch {
+        return raw;
+      }
+    };
+
+    const decrypted = () => {
+      if (!token()) return null;
+      try {
+        // return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo0NjksImV4cCI6MTc2Mjg3NDYxN30=.cWyvBciB5ggrRwjQ/9PbKFxZL0JIIRH8yG/5iKMZY/A=";
+        return decryptFromPHP(token());
+      } catch (err) {
+        console.error("Decryption error:", err);
+        return null;
+      }
+    };
+    const getUserDetailsSuccess = () => {
+      const formData = new FormData();
+      const params = new URLSearchParams(search);
+
+      const aclRoleId = params.get("acl_role_id");
+      const customerId = params.get("customer_id");
+      formData.append("acl_role_id", aclRoleId);
+      formData.append("customer_id", customerId);
+      const token = decrypted();
+      dispatch(getConfirmInfo(token, formData, undefined, setContextLoading));
+    };
+    if (search) {
+      console.log(search, "fromsearch");
+      const formData = new FormData();
+      const params = new URLSearchParams(search);
+
+      const aclRoleId = params.get("acl_role_id");
+      const customerId = params.get("customer_id");
+      formData.append("acl_role_id", aclRoleId);
+      formData.append("customer_id", customerId);
+      const token = decrypted();
+      dispatch(
+        getUserInfoByToken(
+          token,
+          formData,
+          getUserDetailsSuccess,
+          setContextLoading
+        )
+      );
+    }
+
+    return <Outlet />;
+  }
 
   if (!user) {
     return (
