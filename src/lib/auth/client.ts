fdbd98dfe24a5c659d4deb94f164dcbc627c851a
NSaiEPS <sainizameps@gmail.com>
2025-11-14 14:48:39 +0530

    added signout api call

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -174,18 +174,17 @@ class AuthClient {
   }
 
   async signOut(token, formData): Promise<{ error?: string }> {
-    // const res = await fetch(`${BASE_URL}logout`, {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    //   body: formData,
-    // });
+    const res = await fetch(`${BASE_URL}logout`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
 
-    // const data = await res.json();
-    // if (data?.status) {
-    if (true) {
+    const data = await res.json();
+    if (data?.status) {
       clearLocalStorage();
       navigateTo("/login", { replace: true });
     } else {
