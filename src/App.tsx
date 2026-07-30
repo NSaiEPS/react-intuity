dac16de8fbfe0d4f4cd6510e1c748f5e572a4404
joshibhavya64 <joshibhavya64@gmail.com>
2026-07-30 14:56:31 +0530

    version updated

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ const NotFound = React.lazy(() => import("./pages/not-found"));
 
 import { LoaderFallback } from "@/components/core/protectedRoute";
 
-console.log("version 1.1.37")
+console.log("version 1.1.38")
 
 const withSuspense = (element: React.ReactNode) => {
   return (
